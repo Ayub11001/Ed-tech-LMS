3203242d9142d9f83b82f4550e11def714309262
Ayub <sameerbaig690@gmail.com>
2026-09-03 17:56:10 +0530

    add quiz related realtions in schema and add indices

--- server/src/modules/user/dto/update-user.dto.ts
+++ server/src/modules/user/dto/update-user.dto.ts
@@ -1,15 +1,22 @@
-import { IsEmail, IsOptional } from "class-validator";
+import { IsEmail, IsOptional, IsString, Matches, MinLength } from "class-validator";
 
 export class UpdateUserDto {
     @IsEmail()
     @IsOptional()
     email?: string
 
-    @IsEmail()
+    @IsString()
     @IsOptional()
     fullName?: string
     
-    @IsEmail()
-    @IsOptional()
+    @IsString()
+    @MinLength(8, {message: "Password must be atleast 8 characters"})
+    @Matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%.*?&])[A-Za-z\d@$!%*?.&]/,
+        {
+            message:
+            'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+        }
+    )
     password?: string
 }
